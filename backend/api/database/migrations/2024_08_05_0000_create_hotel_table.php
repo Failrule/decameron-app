7935176d38cb6f63bce636517a8dc5129b493307
Failrule <sebitasalguero@gmail.com>
2024-08-05 13:00:02 -0500

    Fix de migraciones de hotel

---
+++ backend/api/database/migrations/2024_08_05_0000_create_hotel_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('hotel', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->string('ciudad');
+            $table->string('nit');
+            $table->integer('cant_habitaciones');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hotel');
+    }
+};
