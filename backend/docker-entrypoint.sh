e4abae5833957ba683d3585675bc1094e7bfebc2
Failrule <sebitasalguero@gmail.com>
2024-08-05 12:39:17 -0500

    Arreglo de incompatibilidad con Docker y Podman

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
+echo "Corriendo entrypoint"
 
-# Salir inmediatamente si un comando falla
-set -e
-
-# Si no existe el archivo composer.json, muestra un mensaje de error y termina
-if [ ! -f "./api/composer.json" ]; then
-    echo "El directorio api no existe. Creando un nuevo proyecto Laravel..."
-    composer create-project laravel/laravel api
-else
-    echo "Ya existe proyecto backend en ./api"
-fi
-
-cd api
+/usr/bin/composer install --prefer-dist --no-scripts --no-dev --optimize-autoloader
 
 # Ejecutar comandos de Artisan
-php artisan key:generate
-php artisan migrate
-php artisan cache:clear
-php artisan config:clear
-php artisan route:clear
+php ./artisan key:generate
+php ./artisan migrate
+php ./artisan cache:clear
+php ./artisan config:clear
+php ./artisan route:clear
 
 # Servir la aplicación
-php artisan serve --port=8000 --host=0.0.0.0
+php ./artisan serve --port=8000 --host=0.0.0.0
 
 exec docker-php-entrypoint "$@"
